45fd1321be70f9a680f059aaa565d64480b646fa
Mark Volpe <mark.tony.volpe@gmail.com>
2019-07-08 20:57:19 -0400

    detail view in progress

--- ChallengeTV/ChallengeTV/UI/DetailsViewController/DetailViewController.swift
+++ ChallengeTV/ChallengeTV/UI/DetailsViewController/DetailViewController.swift
@@ -11,16 +11,134 @@ import UIKit
 class DetailViewController: UIViewController {
 
     public var event:TVEvent? = nil
+    // image constraints
+    @IBOutlet var centeredImageConstraint: NSLayoutConstraint!
+    @IBOutlet var imageViewSpaceToTop: NSLayoutConstraint!
+    var imageToTheLeftTemp:NSLayoutConstraint? = nil
+    var originalImageTopSpace:CGFloat = 0.0
+    
+    // details view constraints
+    @IBOutlet var detailsCentered: NSLayoutConstraint!
+    @IBOutlet var detailsHeightRatio: NSLayoutConstraint!
+    @IBOutlet var detailsWidthEqualToSuper: NSLayoutConstraint!
+    @IBOutlet var detailsTopToThumbnail: NSLayoutConstraint!
+    
+    // details view runtime constraints...
+    var detailsLeftToImageRight:NSLayoutConstraint? = nil
+    var detailsRightToButtonLeft:NSLayoutConstraint? = nil
+    var detailsTopToSuperTop:NSLayoutConstraint? = nil
+    var detailsHeightEqualToImageView:NSLayoutConstraint? = nil
+    
+    @IBOutlet var closeButton: UIButton!
+    @IBOutlet var thumbNail: UIImageView!
+    @IBOutlet var detailsView: UIView!
+    
+    @IBOutlet var detailsHeading: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        createRuntimeLandscapeConstraints()
 
         if let event = self.event{
             populateView(event: event)
         }
+        
+        handleRotation()
     }
     
     func populateView(event:TVEvent){
+        thumbNail.sd_setImage(with: event.thumbnailUrl) { (image, error, type, url) in
+            
+        }
+    }
+    
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+        handleRotation()
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         
+        view.setNeedsUpdateConstraints()
+    }
+}
+
+// MARK: Constraint management
+extension DetailViewController{
+    func createRuntimeLandscapeConstraints(){
+        // for the thumbnail
+        if imageToTheLeftTemp == nil{
+            imageToTheLeftTemp = thumbNail.leftAnchor.constraint(
+                equalTo: view.leftAnchor
+            )
+        }
+        
+        // for the details view
+        if detailsLeftToImageRight == nil{
+            detailsLeftToImageRight = detailsView.leftAnchor.constraint(
+                equalTo: thumbNail.rightAnchor
+            )
+        }
+        
+        if detailsRightToButtonLeft == nil{
+            detailsRightToButtonLeft = detailsView.rightAnchor.constraint(
+                equalTo: closeButton.leftAnchor
+            )
+        }
+        
+        if detailsTopToSuperTop == nil{
+            detailsTopToSuperTop = detailsView.topAnchor.constraint(
+                equalTo: view.topAnchor
+            )
+        }
+        
+        if detailsHeightEqualToImageView == nil{
+            detailsHeightEqualToImageView = detailsView.heightAnchor.constraint(
+                equalTo: thumbNail.heightAnchor
+            )
+        }
+    }
+    
+    func set(landscapeMode:Bool){
+
+        if landscapeMode{
+            set(portraitMode: false)
+        }
+        // true for landscape
+        imageToTheLeftTemp?.isActive = landscapeMode
+        detailsLeftToImageRight?.isActive = landscapeMode
+        detailsRightToButtonLeft?.isActive = landscapeMode
+        detailsTopToSuperTop?.isActive = landscapeMode
+        detailsHeightEqualToImageView?.isActive = landscapeMode
+        
+        if !landscapeMode{
+            set(portraitMode: true)
+        }
+    }
+    
+    func set(portraitMode:Bool){
+        detailsHeightRatio.isActive = portraitMode
+        detailsTopToThumbnail.isActive = portraitMode
+        detailsWidthEqualToSuper.isActive = portraitMode
+        detailsCentered.isActive = portraitMode
+        centeredImageConstraint.isActive = portraitMode
+    }
+    
+    func handleRotation(){
+        if UIDevice.current.orientation == UIDeviceOrientation.landscapeLeft || UIDevice.current.orientation == UIDeviceOrientation.landscapeRight{
+            // if we're in landscape mode...
+            // move the thumbnail to the top - save the original location
+            originalImageTopSpace = imageViewSpaceToTop.constant
+            imageViewSpaceToTop.constant = 0
+            set(landscapeMode: true)
+        }else{
+            imageViewSpaceToTop.constant = originalImageTopSpace
+            set(landscapeMode: false)
+
+        }
+        self.view.updateConstraints()
+        self.view.setNeedsLayout()
+        self.view.layoutIfNeeded()
     }
 }
